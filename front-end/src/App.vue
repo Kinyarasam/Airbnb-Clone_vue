11664f03ddc3095b949cd25ad140ae59bf11da39
kinyarasam <samkinyara2000@gmail.com>
2022-05-21 17:42:36 +0300

    Top Nav Bar.

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -2,10 +2,12 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import TopNav from './components/TaskBar.vue'
 </script>
 
 <template>
-  <div class="bg-rose-600">
+  <div class="max-h-screen overflow-hidden">
+    <TopNav />
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Hello Vue 3 + Vite" />
   </div>
---
+++ front-end/src/components/TaskBar.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="shadow bg-gray-100">
+        <div class="py-2 grid grid-cols-3 text-gray-900 gap-5 justify-between items-center">
+            <div></div>
+            <div class="font-bold text-xl">
+                Introducing the new Airbnb
+            </div>
+            <div class="flex text-sm tracking-tight">
+                <div class="flex justify-center items-center hover:bg-white rounded p-2 ">
+                    <img class="bg-rose-500 h-7 rounded w-14 overflow-hidden" src="" alt="film"/>
+                    <span class="px-4">Play the film</span>
+                </div>
+                <span class="border-l mx-2"></span>
+                <div class="hover:bg-white rounded flex items-center px-4">
+                    Learn more
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
